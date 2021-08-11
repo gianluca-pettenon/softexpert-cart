569b527e16d4141563c767babc2570adcdec294a
GIANLUCA PETTENON <gianluca.pettenon@ielusc.br>
2021-08-11 10:29:00 -0300

    Added ProductModel

--- App/Libraries/Response.php
+++ App/Libraries/Response.php
@@ -1,8 +1,41 @@
 <?php
 
-namespace App\Helper;
+namespace App\Libraries;
 
 class Response
 {
+    public function __construct()
+    {
+        $this->response = [];
+    }
+
+    public function setMessage($message)
+    {
+        $this->response['message'] = $message;
+    }
+
+    public function setUri($uri)
+    {
+        $this->response['uri'] = $uri;
+    }
+
+    public function setData($data)
+    {
+        $this->response['data'] = $data;
+    }
+
+    public function setClass($class)
+    {
+        $this->response['class'] = $class;
+    }
+
+    public function getResponse()
+    {
+        if (!headers_sent()) :
+            header('Content-Type: application/json');
+        endif;
+
+        exit(json_encode($this->response));
+    }
     
 }
--- App/Model/ProductModel.php
+++ App/Model/ProductModel.php
@@ -8,15 +8,28 @@ class ProductModel
 {
 
     private $db;
+    private $name;
+    private $description;
+    private $price;
 
     public function __construct()
     {
         $this->db = new Database;
     }
 
-    public function test()
+    public function setName(string $name)
     {
-        return var_dump($this->db->getConnection());
+        $this->name = $name;
+    }
+
+    public function setDescription(string $description)
+    {
+        $this->description = $description;
+    }
+
+    public function setPrice(float $price)
+    {
+        $this->price = $price;
     }
 
 }
