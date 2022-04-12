8cfe05694f4ef63b1d4bce344f12f03878d07593
Gianluca Pettenon <gpettenon@softexpert.com>
2022-04-12 18:05:58 -0300

    Organização no diretório de teste

--- App/Test/Helper/InputTest.php
+++ App/Test/Helper/InputTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Test;
+namespace App\Test\Helper;
 
 use PHPUnit\Framework\TestCase;
 
--- App/Test/Libraries/ResponseTest.php
+++ App/Test/Libraries/ResponseTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Test;
+namespace App\Test\Libraries;
 
 use PHPUnit\Framework\TestCase;
 
